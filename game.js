74b42d059223619095e3fd3396bddb225744c498
Samuel Familusi <sfamilusi51@outlook.com>
2022-04-12 10:24:28 +0200

    Remove cards which have been placed TODO : fix card alignment every frame

--- game.js
+++ game.js
@@ -9,13 +9,13 @@ width = 240
 height = 136
 
 function lerp(a, b, t) { return (1 - t) * a + t * b }
-function pal(c0,c1) {
-	if (c0==undefined && c1==undefined) {
-		for (i=0;i<=15;i++) {
-			poke4(0x3FF0*2+i,i)
+function pal(c0, c1) {
+	if (c0 == undefined && c1 == undefined) {
+		for (i = 0; i <= 15; i++) {
+			poke4(0x3FF0 * 2 + i, i)
 		}
 	}
-	else poke4(0x3FF0*2+c0,c1)
+	else poke4(0x3FF0 * 2 + c0, c1)
 }
 
 function remove(array, index) {
@@ -90,31 +90,31 @@ Collision = {
 pile = [] // the pile which the players draw from
 
 suites = ["O", "#", "+", "*", "^"] // each type of card
-nums = [1,2,3,4,5,7,8,10,11,12,13,14] // each possible card number
+nums = [1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 14] // each possible card number
 not_cards = [ // cards which shouldn't exist
-	["+",4],["#",4],
-	["+",8],["#",8],
-	["+",12],["#",12],
-	["*",10],["*",11],["*",12],["*",13],["*",14]
+	["+", 4], ["#", 4],
+	["+", 8], ["#", 8],
+	["+", 12], ["#", 12],
+	["*", 10], ["*", 11], ["*", 12], ["*", 13], ["*", 14]
 ]
 // fill the pile with all the cards
 for (j = 0; j < suites.length; j++) {
 	suite = suites[j]
 	for (i = 0; i < nums.length; i++) {
 		c = [suite, nums[i]]
-        allowed = true
+		allowed = true
 		// chesk if card is not allowed
-        for (k = 0; k < not_cards.length; k++) {
-            if (c[0] == not_cards[k][0] && c[1] == not_cards[k][1]) {
-                allowed = false
-            }
-        }
+		for (k = 0; k < not_cards.length; k++) {
+			if (c[0] == not_cards[k][0] && c[1] == not_cards[k][1]) {
+				allowed = false
+			}
+		}
 		if (allowed) pile.push(c)
 	}
 }
 // add the whot cards
 for (i = 0; i < 5; i++) {
-	pile.push(["W",20])
+	pile.push(["W", 20])
 }
 pile = shuffle(pile)
 
@@ -124,14 +124,15 @@ function Card(pos) {
 	this.target_pos = pos
 	this.siz = Point(8 * 3, 8 * 4)
 	this.value = pile.pop()
-
+	this.hover = false
 	this.update = function () {
-		if (Collision.point_rect(mouse.pos, this) && mouse.l) { // if the mouse is holding this card
+		this.hover = Collision.point_rect(mouse.pos, this)
+		if (this.hover && mouse.l) { // if the mouse is holding this card
 			this.pos = add_Point(mouse.pos, Point(-8, -8))
-			if (Collision.rect_rect(this,stack_button)) {
+		} else if (this.hover) {
+			if (Collision.rect_rect(this, stack_button)) {
 				stack_button.add(this.value)
 			}
-		} else if (Collision.point_rect(mouse.pos, this)) {
 			this.pos.y = this.target_pos.y - 8
 		} else {
 			this.pos = lerp_Point(this.pos, this.target_pos, 0.07)
@@ -153,63 +154,75 @@ start_cards_length = 6 // the number of cards each player starts with
 cards = []
 for (i = 0; i < start_cards_length; i++) {
 	cards.push(
-		new Card(Point(45 + (i * 16), 77+i))
+		new Card(Point(45 + (i * 16), 77 + i))
 	)
 }
 
 stack = [ // the pile which players add to
 	pile.pop()
 ]
+remove_value = null
 stack_button = {
-	pos : Point((width*(2/3))-4,(height/3)-6),
-	siz : Point(8 * 3, 8 * 4),
-	add : function (value) {
-		top_card = stack[stack.length-1]
+	pos: Point((width * (2 / 3)) - 4, (height / 3) - 6),
+	siz: Point(8 * 3, 8 * 4),
+	add: function (value) {
+		top_card = stack[stack.length - 1]
 		if (value[0] == top_card[0] || value[1] == top_card[1]) {
 			stack.push(value)
+			remove_value = value // value of card to be removed
 			return true
 		} else {
 			return false
 		}
 	},
-	draw : function() {
+	update: function () {
+		if (remove_value) {
+			for (i = 0; i < cards.length; i++) {
+				if ((cards[i].value[0] == remove_value[0]) && (cards[i].value[1] == remove_value[1])) {
+					cards = remove(cards, i)
+				}
+			}
+		}
+		remove_value = null // reset the value for the next frame
+	},
+	draw: function () {
 		map(30, 0, 3, 4, this.pos.x, this.pos.y, 0)
 		font(
-			stack[stack.length-1][0] + "\n" + stack[stack.length-1][1],
-			this.pos.x+5,this.pos.y+4
+			stack[stack.length - 1][0] + "\n" + stack[stack.length - 1][1],
+			this.pos.x + 5, this.pos.y + 4
 		)
 	}
 }
 
-function Button(pos,label,callback) {
+function Button(pos, label, callback) {
 	this.pos = pos
 	this.label = label
 	this.callback = callback // function which is called when pressed
 	this.siz = Point(8 * 2, 8 * 2)
 	this.pressed = false
 	this.update = function () {
-		this.just_pressed = (this.pressed == false && Collision.point_rect(mouse.pos,this) && mouse.l)
-		
+		this.just_pressed = (this.pressed == false && Collision.point_rect(mouse.pos, this) && mouse.l)
+
 		if (this.just_pressed) {
 			this.callback()
 			this.pressed = true
 		}
 
-		if (this.pressed && (mouse.l == false)) {this.pressed = false}
+		if (this.pressed && (mouse.l == false)) { this.pressed = false }
 	}
 	this.draw = function () {
-		spr(this.pressed ? 94 : 92, this.pos.x,this.pos.y, 2,1,0,0,2,2)
-		pal(1,0)
-		font(this.label,this.pos.x+4,this.pos.y+ (this.pressed ? 6 : 4))
+		spr(this.pressed ? 94 : 92, this.pos.x, this.pos.y, 2, 1, 0, 0, 2, 2)
+		pal(1, 0)
+		font(this.label, this.pos.x + 4, this.pos.y + (this.pressed ? 6 : 4))
 		pal()
 	}
 }
 
 draw_button = new Button(
-	Point(width/3,height/3),
+	Point(width / 3, height / 3),
 	"D",
 	function () {
-		cards.push(new Card(Point(45 + (cards.length * 16), 77+cards.length)))
+		cards.push(new Card(Point(45 + (cards.length * 16), 77 + cards.length)))
 	}
 )
 
@@ -244,8 +257,9 @@ function TIC() {
 	for (i = 0; i < cards.length; i++) {
 		cards[i].update()
 	}
-	
-	map(33, 0, 3, 4, draw_button.pos.x-4, draw_button.pos.y-6, 0)
+	stack_button.update()
+
+	map(33, 0, 3, 4, draw_button.pos.x - 4, draw_button.pos.y - 6, 0)
 	draw_button.draw()
 	stack_button.draw()
 	//draw all cards
